6b27d1a555f7cdc1fe4f7c9cac4c2bd9280babbf
vikonikus <vikonik@mail.ru>
2025-08-12 21:29:30 +0400

    Перенос кода с рекордера Katalon. Добавление зщависимости в Gradle.

---
+++ src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -0,0 +1,65 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import java.time.Duration;
+
+public class GroupCreationTests {
+  WebDriver driver;
+
+  @BeforeClass
+  public void setUp() throws Exception {
+    driver = new EdgeDriver();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+  }
+
+  @Test
+  public void testCreationGroup() throws Exception {
+    driver.get("http://localhost/addressbook/group.php");
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.xpath("//input[@value='Login']")).click();
+    driver.findElement(By.linkText("groups")).click();
+    driver.findElement(By.name("new")).click();
+    driver.findElement(By.name("group_name")).click();
+    driver.findElement(By.name("group_name")).clear();
+    driver.findElement(By.name("group_name")).sendKeys("test1");
+    driver.findElement(By.name("group_header")).click();
+    driver.findElement(By.name("group_header")).clear();
+    driver.findElement(By.name("group_header")).sendKeys("test2");
+    driver.findElement(By.name("group_footer")).click();
+    driver.findElement(By.name("group_footer")).clear();
+    driver.findElement(By.name("group_footer")).sendKeys("test3");
+    driver.findElement(By.name("submit")).click();
+    driver.findElement(By.linkText("group page")).click();
+  }
+
+  @AfterClass
+  public void tearDown() throws Exception {
+    driver.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+}
