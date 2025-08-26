899affabea45dfee4aa35a9793ee708794aa0b26
vikonikus <vikonik@mail.ru>
2025-08-26 23:47:09 +0400

    Модифицирован тест на создание группы с помощью лямбды выражения

--- src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,27 +4,31 @@ import java.util.Objects;
 
 public class GroupData {
 
-  private final String id;
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public String getId() {
+  public int getId() {
     return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getName() {
@@ -43,7 +47,7 @@ public class GroupData {
   public boolean equals(Object o) {
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name);
+    return id == groupData.id && Objects.equals(name, groupData.name);
   }
 
   @Override
--- src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,9 +1,9 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
-
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -12,9 +12,15 @@ public class GroupCreationTests extends TestBase {
   public void testCreationGroup() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    GroupData group = new GroupData("test2", null, null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
   }
 
