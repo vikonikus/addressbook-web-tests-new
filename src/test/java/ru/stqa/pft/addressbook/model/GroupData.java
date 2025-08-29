65f998b234bbbc3acbe500efc7f2e22d4043527e
vikonikus <vikonik@mail.ru>
2025-08-30 01:15:01 +0400

    Произведено изменение кода для создания, удаления и модификации групп по Id

--- src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -18,6 +18,18 @@ public class GroupData {
     return this;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id && Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name);
+  }
+
   public GroupData withName(String name) {
     this.name = name;
     return this;
@@ -43,19 +55,6 @@ public class GroupData {
 
   public String getFooter() {
     return footer;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(name);
   }
 
   @Override
--- src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,38 +4,33 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupModificationTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
     app.goTo().groupPage();
-    if (app.group().list().isEmpty()) {
+    if (app.group().all().isEmpty()) {
       app.group().create(new GroupData().withName("test1"));
     }
   }
 
   @Test
   public void testGroupModification() {
-    List<GroupData> before = app.group().list();
-    int index = before.size() - 1;
+    Set<GroupData> before = app.group().all();
+    GroupData modifiedGroup = before.iterator().next();
     GroupData group = new GroupData()
-            .withId(before.get(index).getId())
+            .withId(modifiedGroup.getId())
             .withName("test1")
             .withHeader("test2")
             .withFooter("test3");
-    app.group().modify(index, group);
-    List<GroupData> after = app.group().list();
+    app.group().modify(group);
+    Set<GroupData> after = app.group().all();
     Assert.assertEquals(after.size(), before.size());
 
-    before.remove(index);
+    before.remove(modifiedGroup);
     before.add(group);
-    Comparator<? super GroupData> byId =
-            (group1, group2) -> Integer.compare(group1.getId(), group2.getId());
-    before.sort(byId);
-    after.sort(byId);
     Assert.assertEquals(before, after);
   }
 
