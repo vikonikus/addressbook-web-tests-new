0d8df9aed585e46c6a7511317fd5908e7837b360
vikonikus <vikonik@mail.ru>
2025-08-30 12:07:44 +0400

    Скорректирована строка для хеширования проверки на сравнение количества элементов в группе в тесте на удаление группы (передвинута выше строки создания групп)

--- src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -23,8 +23,8 @@ public class GroupDeletionTests extends TestBase {
     Groups before = app.group().all();
     GroupData deletedGroup = before.iterator().next();
     app.group().delete(deletedGroup);
-    Groups after = app.group().all();
     assertThat(app.group().count(), equalTo(before.size() - 1));
+    Groups after = app.group().all();
     assertThat(after, equalTo(before.without(deletedGroup)));
   }
 
