84d29def90ab78f9371327439bb7a8a715d1ff19
vikonikus <vikonik@mail.ru>
2025-09-01 00:21:44 +0400

    Клеим строки с номерами телефонов из формы редактирования контактов методом обратных проверок.

--- src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -13,6 +13,19 @@ public class ContactData {
   private String workPhone;
 //  private String email;
   private String group;
+  private String allPhones;
+
+
+  public String getAllPhones() {
+    return allPhones;
+  }
+
+  public ContactData withAllPhones(String allPhones) {
+    this.allPhones = allPhones;
+    return this;
+  }
+
+
 
   public ContactData withId(int id) {
     this.id = id;
--- src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -2,6 +2,10 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -13,12 +17,18 @@ public class ContactPhoneTests extends TestBase {
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
-    assertThat(contact.getHomePhone(), equalTo(cleaned(contactInfoFromEditForm.getHomePhone())));
-    assertThat(contact.getMobilePhone(), equalTo(cleaned(contactInfoFromEditForm.getMobilePhone())));
-    assertThat(contact.getWorkPhone(), equalTo(cleaned(contactInfoFromEditForm.getWorkPhone())));
+    assertThat(contact.getAllPhones(), equalTo(mergePhone(contactInfoFromEditForm)));
   }
 
-  public String cleaned(String phone) {
+  private String mergePhone(ContactData contact) {
+    return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
+            .stream()
+            .filter((s) -> ! s.equals(""))
+            .map(ContactPhoneTests::cleaned)
+            .collect(Collectors.joining("\n"));
+  }
+
+  public static String cleaned(String phone) {
     return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
   }
 }
