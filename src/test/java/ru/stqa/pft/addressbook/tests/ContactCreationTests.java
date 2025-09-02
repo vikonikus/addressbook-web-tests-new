6dfbdd3b892e3e0be467cb92423e75a12fdf31b3
vikonikus <vikonik@mail.ru>
2025-09-02 20:54:59 +0400

    Добавление файла/фото в создание контакта по абсолютному пути с помощью метода getAbsolutePath

--- src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -12,21 +15,17 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() throws Exception {
     app.goTo().gotoHomePage();
     Contacts before = app.contact().all();
+    File photo = new File("src/test/resources/floor.png");
     ContactData contact = new ContactData()
             .withFirstname("firstName1")
             .withLastname("LastName1")
-            .withHomePhone("111")
-            .withMobilePhone("222")
-            .withWorkPhone("333")
-            .withAddress("Samara")
-            .withFirstEmail("1@1.ru")
-            .withSecondEmail("2@2.ru")
-            .withThirdEmail("3@2.ru");
+            .withPhoto(photo);
     app.contact().create(contact);
     Contacts after = app.contact().all();
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt(ContactData::getId).max().getAsInt()))));
   }
+
 
 }
