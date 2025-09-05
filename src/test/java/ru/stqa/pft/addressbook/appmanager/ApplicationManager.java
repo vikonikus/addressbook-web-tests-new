0899f6e20a3e714b1e492cbe8ed4faa30577869e
vikonikus <vikonik@mail.ru>
2025-09-06 00:52:09 +0400

    Вынесено описание тестового стенда в отдельный файл local.properties

--- src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -4,10 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.Browser;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class ApplicationManager {
+  private final Properties properties;
   WebDriver wd;
 
   private NavigationHelper navigationHelper;
@@ -17,11 +20,14 @@ public class ApplicationManager {
   public boolean acceptNextAlert;
   private String browser;
 
-  public ApplicationManager(String browser) {
+  public ApplicationManager(String browser) throws IOException {
     this.browser = browser;
+    properties = new Properties();
   }
 
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
     if (browser.equals("edge")) {
       wd = new EdgeDriver();
     } else if (browser.equals("chrome")) {
@@ -33,14 +39,13 @@ public class ApplicationManager {
     }
 
     wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
-    wd.get("http://localhost/addressbook");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     contactHelper = new ContactHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
   }
-
 
   public void stop() {
     wd.quit();
--- src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,10 +5,19 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 public class TestBase {
 
-  protected static final ApplicationManager app
-          = new ApplicationManager(System.getProperty("browser", "firefox"));
+  protected static final ApplicationManager app;
+
+  static {
+    try {
+      app = new ApplicationManager(System.getProperty("browser", "firefox"));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   @BeforeSuite
   public void setUp() throws Exception {
